bdcdcd9bc96a9399a26f50f2bf4ab7f37c815201
James Moore <james@knowthen.com>
2019-07-12 10:52:40 -0700

    0650_first_app_view_body

--- vote/lib/election.ex
+++ vote/lib/election.ex
@@ -13,4 +13,31 @@ defmodule Election do
       "Election for: #{election.name}\n"
     ]
   end
+  
+  def view_body(election) do
+    election.candidates
+    |> sort_candidates_by_votes_desc()
+    |> candidates_to_strings()
+    |> prepend_candidates_header()
+  end
+  
+  defp prepend_candidates_header(candidates) do
+    [
+      "ID\tVotes\tName\n",
+      "-----------------------------\n"
+      | candidates
+    ]
+  end
+  
+  defp candidates_to_strings(candidates) do
+    candidates
+    |> Enum.map(fn %{id: id, name: name, votes: votes} ->
+      "#{id}\t#{votes}\t#{name}\n"
+    end) 
+  end
+  
+  defp sort_candidates_by_votes_desc(candidates) do
+    candidates
+    |> Enum.sort(&(&1.votes >= &2.votes))
+  end
 end
