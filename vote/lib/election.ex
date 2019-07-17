268b64a31e985d9ca06bdb20f6640b67be6ba598
James Moore <james@knowthen.com>
2019-07-17 16:14:53 -0700

    0660_first_app_view_footer_and_view_composition

--- vote/lib/election.ex
+++ vote/lib/election.ex
@@ -21,6 +21,21 @@ defmodule Election do
     |> prepend_candidates_header()
   end
   
+  def view_footer() do
+    [
+      "\n",
+      "commands: (n)ame <election>, (a)dd <candidate>, (v)ote <id>, (q)uit\n"
+    ]
+  end
+  
+  def view(election) do
+    [
+      view_header(election),
+      view_body(election),
+      view_footer()
+    ]
+  end
+  
   defp prepend_candidates_header(candidates) do
     [
       "ID\tVotes\tName\n",
