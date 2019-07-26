6f9972c7c4b687e2a2141a36a66af46910ece9ae
James Moore <james@knowthen.com>
2019-07-26 16:11:31 -0700

    0750_processes_long_short_lived

---
+++ lectures/processes/process_short_lived.exs
@@ -0,0 +1,43 @@
+voting_machine_ips = [
+  "10.0.1.1",
+  "10.0.1.2",
+  "10.0.1.3",
+  "10.0.1.4",
+  "10.0.1.5",
+  "10.0.1.6",
+  "10.0.1.7",
+  "10.0.1.8",
+  "10.0.1.9",
+  "10.0.1.10"
+]
+
+simulated_api_call = fn _ip ->
+  votes = Enum.random(225..275)
+  :timer.sleep(votes)
+  votes
+end
+
+defmodule Worker do
+  def start(fun) do
+    pid = self()
+    spawn(fn ->
+      send(pid, {self(), fun.()})
+    end)
+  end
+  
+  def wait(pid) do
+    receive do
+      {^pid, result} -> result
+    end
+  end
+end
+
+voting_machine_ips
+|> Enum.map(fn ip -> Worker.start(fn -> simulated_api_call.(ip) end) end)
+|> IO.inspect()
+|> Enum.map(&Worker.wait/1)
+|> Enum.sum()
+|> IO.inspect()
+
+:timer.sleep(300)
+Process.info(self(), :messages) |> IO.inspect()
