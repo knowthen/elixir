26b7df66fecb9dd71edaf5af76c74a148efa5af0
James Moore <james@knowthen.com>
2019-08-09 16:01:57 -0700

    0880 OTP Supervisor

---
+++ counter/lib/counter_supervisor.ex
@@ -0,0 +1,16 @@
+defmodule CounterSupervisor do
+  use Supervisor
+  
+  def start_link(init_args) do
+    Supervisor.start_link(__MODULE__, init_args)
+  end
+  
+  def init(init_args) do
+    children = [
+      {Counter, init_args}
+    ]
+    
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+  
+end
