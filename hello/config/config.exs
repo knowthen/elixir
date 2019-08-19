114266cbc85330e8ce556c39b1d62f336ade13ee
James Moore <james@knowthen.com>
2019-08-19 15:39:07 -0700

    hello phoenix project initial commit

---
+++ hello/config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :hello,
+  ecto_repos: [Hello.Repo]
+
+# Configures the endpoint
+config :hello, HelloWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "ZvXtd9nRwtwcsm7xjKdvh8KUHkPs2kpzEqMQEW2zr67utY2GNKpqAGnpaWWlekb9",
+  render_errors: [view: HelloWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Hello.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ hello/lib/hello_web/router.ex
@@ -0,0 +1,26 @@
+defmodule HelloWeb.Router do
+  use HelloWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", HelloWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", HelloWeb do
+  #   pipe_through :api
+  # end
+end
