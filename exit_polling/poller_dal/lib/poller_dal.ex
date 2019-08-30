108cecf802aaad66586c9fd3f843eea0842188a7
James Moore <james@knowthen.com>
2019-08-30 15:48:38 -0700

    1110 Create Poller Dal Project and install deps

---
+++ exit_polling/poller_dal/lib/poller_dal.ex
@@ -0,0 +1,18 @@
+defmodule PollerDal do
+  @moduledoc """
+  Documentation for PollerDal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PollerDal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ exit_polling/poller_dal/test/poller_dal_test.exs
@@ -0,0 +1,8 @@
+defmodule PollerDalTest do
+  use ExUnit.Case
+  doctest PollerDal
+
+  test "greets the world" do
+    assert PollerDal.hello() == :world
+  end
+end
