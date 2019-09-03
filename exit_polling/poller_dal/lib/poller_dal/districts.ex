5b19a493062f0113031d85e58c86138bb269b640
James Moore <james@knowthen.com>
2019-09-03 12:17:03 -0700

    1150 districts crud api

---
+++ exit_polling/poller_dal/lib/poller_dal/districts.ex
@@ -0,0 +1,25 @@
+defmodule PollerDal.Districts do
+  alias PollerDal.Repo
+  alias PollerDal.Districts.District
+  
+  def create_district(attrs) do
+    %District{}
+    |> District.changeset(attrs)
+    |> Repo.insert()
+  end
+  
+  def update_district(%District{} = district, attrs) do
+    district
+    |> District.changeset(attrs)
+    |> Repo.update()
+  end
+  
+  def delete_district(%District{} = district) do
+    Repo.delete(district)
+  end
+  
+  def list_districts, do: Repo.all(District)
+  
+  def get_district!(id), do: Repo.get!(District, id)
+  
+end
