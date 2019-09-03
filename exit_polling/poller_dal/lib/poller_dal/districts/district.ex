a8f4a0eedb2bbeb2869eff1d43516fd20da2db15
James Moore <james@knowthen.com>
2019-09-03 11:51:11 -0700

    1140 poller_dal changeset

--- exit_polling/poller_dal/lib/poller_dal/districts/district.ex
+++ exit_polling/poller_dal/lib/poller_dal/districts/district.ex
@@ -1,9 +1,17 @@
 defmodule PollerDal.Districts.District do
   use Ecto.Schema
+  import Ecto.Changeset
   
   schema "districts" do
     field(:name, :string)
     
     timestamps()
+  end
+  
+  def changeset(district, attrs) do
+    district
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2)
   end
 end
