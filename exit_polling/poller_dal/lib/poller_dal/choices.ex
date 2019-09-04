627e67e2412dda73862402d4b68ce1c6a185d819
James Moore <james@knowthen.com>
2019-09-04 09:12:47 -0700

    prep choices

---
+++ exit_polling/poller_dal/lib/poller_dal/choices.ex
@@ -0,0 +1,19 @@
+defmodule PollerDal.Choices do
+
+  # TODO: write `list_choices/0` function
+
+  # TODO: write `list_choices_by_question_id/1` function
+
+  # TODO: write `list_choices_by_choice_ids/1` function
+  
+  # TODO: write `get_choice!/1` function
+
+  # TODO: write `create_choice/1` function
+
+  # TODO: write `update_choice/1` function
+
+  # TODO: write `delete_choice/1` function
+
+  # TODO: write `change_choice/1` function
+  
+end
