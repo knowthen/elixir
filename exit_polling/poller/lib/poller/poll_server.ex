677b344d0486ecc1b794185302ab6cb71918604c
James Moore <james@knowthen.com>
2019-09-20 14:48:09 -0700

    1370 periodicly saving votes

--- exit_polling/poller/lib/poller/poll_server.ex
+++ exit_polling/poller/lib/poller/poll_server.ex
@@ -1,7 +1,9 @@
 defmodule Poller.PollServer do
   use GenServer
   alias Poller.Poll
-  alias PollerDal.Questions
+  alias PollerDal.{Questions, Choices}
+  
+  @save_time 10 * 60 * 1000
   
   def start_link(district_id) do
     name = district_name(district_id)
@@ -28,6 +30,7 @@ defmodule Poller.PollServer do
   # Callbacks
   
   def init(district_id) do
+    schedule_save()
     poll = init_poll(district_id)
     {:ok, poll}
   end
@@ -53,5 +56,29 @@ defmodule Poller.PollServer do
   def handle_call(:get, _from, poll) do
     {:reply, poll, poll}
   end
+  
+  def save_votes(poll) do
+    poll.votes
+    |> Map.keys()
+    |> Choices.list_choices_by_choice_ids()
+    |> Enum.each(fn choice ->
+      current_votes = Map.get(poll.votes, choice.id, choice.votes)
+      
+      if current_votes != choice.votes do
+        Choices.update_choice(choice, %{votes: current_votes})
+      end
+    end)
+  end
+  
+  def schedule_save(), do: Process.send_after(self(), :save, @save_time)
+  
+  def handle_info(:save, poll) do
+    save_votes(poll)
+    
+    schedule_save()
+    {:noreply, poll}
+  end
+  
+  def terminate(_reason, poll), do: save_votes(poll)
   
 end
